8175687e63f6f3ebb03d002999bab0fa024de31d
zhangbw <yuge1805@yeah.net>
2023-11-25 14:46:05 +0800

    feat: init sentinel

---
+++ dashboard/start.bat
@@ -0,0 +1,2 @@
+@echo off
+cmd /k  java -Dserver.port=8080 -Dcsp.sentinel.dashboard.server=localhost:8080 -Dproject.name=sentinel-dashboard -jar sentinel-dashboard-1.8.6.jar
---
+++ portal/portal-server/src/main/java/com/yuge/ing/sentinel/portal/controller/PortalController.java
@@ -0,0 +1,20 @@
+package com.yuge.ing.sentinel.portal.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: yuge
+ * @date: 2023/11/23
+ **/
+@RestController
+@RequestMapping("/portal")
+public class PortalController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello portal!";
+    }
+
+}
